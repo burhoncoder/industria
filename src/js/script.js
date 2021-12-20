0e13246af03b03740dfa42ae1caaeb4fc9eda5e7
Burhon <megacoder77@gmail.com>
2021-12-20 10:38:59 +0500

    add dist folder

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,48 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   autoplay: {
     disableOnInteraction: false,
     delay: 2000,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 });
-const header = document.querySelector('.header');
-const toTop = document.querySelector('.to-top');
-toTop.addEventListener('click', (e) => {
-  header.scrollIntoView({ block: 'start', behavior: 'smooth' });
+const header = document.querySelector(".header");
+const toTop = document.querySelector(".to-top");
+toTop.addEventListener("click", (e) => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-const burgerOpen = document.querySelector('.burger-icon');
-const burgerClose = document.querySelector('.burger__close');
-const burgerMenu = document.querySelector('.burger__menu');
-const burgerOverlay = document.querySelector('.burger__overlay');
-burgerOpen.addEventListener('click', (e) => {
-  burgerMenu.classList.add('burger__menu--active');
-  burgerOverlay.classList.add('burger__overlay--active');
+const burgerOpen = document.querySelector(".burger-icon");
+const burgerClose = document.querySelector(".burger__close");
+const burgerMenu = document.querySelector(".burger__menu");
+const burgerOverlay = document.querySelector(".burger__overlay");
+burgerOpen.addEventListener("click", (e) => {
+  burgerMenu.classList.add("burger__menu--active");
+  burgerOverlay.classList.add("burger__overlay--active");
 });
-burgerClose.addEventListener('click', (e) => {
-  burgerMenu.classList.remove('burger__menu--active');
-  burgerOverlay.classList.remove('burger__overlay--active');
+burgerClose.addEventListener("click", (e) => {
+  burgerMenu.classList.remove("burger__menu--active");
+  burgerOverlay.classList.remove("burger__overlay--active");
 });
-burgerOverlay.addEventListener('click', (e) => {
-  burgerMenu.classList.remove('burger__menu--active');
-  burgerOverlay.classList.remove('burger__overlay--active');
+burgerOverlay.addEventListener("click", (e) => {
+  burgerMenu.classList.remove("burger__menu--active");
+  burgerOverlay.classList.remove("burger__overlay--active");
 });
 
-const nav = header.querySelectorAll('.nav__item');
-const search = document.querySelector('.nav__search');
-const searchI = document.querySelector('.nav__searchI');
-const searchInput = document.querySelector('.nav__search-input');
-searchI.addEventListener('click', (e) => {
+const nav = header.querySelectorAll(".nav__item");
+const search = document.querySelector(".nav__search");
+const searchI = document.querySelector(".nav__searchI");
+const searchInput = document.querySelector(".nav__search-input");
+searchI.addEventListener("click", (e) => {
   nav.forEach((item) => {
-    item.classList.toggle('nav__item--hidden');
+    item.classList.toggle("nav__item--hidden");
   });
-  search.classList.toggle('nav__search--active');
+  search.classList.toggle("nav__search--active");
   searchInput.focus();
-  searchInput.value = '';
+  searchInput.value = "";
 });
